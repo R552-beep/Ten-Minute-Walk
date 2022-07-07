451ea8037a72f01f687e3771760549526c796d83
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-07-07 12:58:26 +0100

    Added method and test ten minute, all tests passed

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -1,7 +1,17 @@
-def ten_minute_walk?(walk)
-  if walk.count == 10
-    true
+def ten_minute_walk?(walk)  
+
+  if walk.length. == 10  
+    north = walk.count('n')
+    south = walk.count('s')
+    east = walk.count('e')
+    west = walk.count('w')
+    north != south || east != west ? false : true
   else
     false
   end
 end
+
+
+
+
+
--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -1,10 +1,21 @@
 require 'ten_minute_walk'
 
 describe "ten minute walk?" do
+
   it "should return true if walk count is length is 10" do
     expect(ten_minute_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq true
   end
 
+  it "should return false if the walk count is less than 10" do
+  expect(ten_minute_walk?(['n', 's', 'w', 'e'])).to eq false
+  end
 
+  it " should return (['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w']) true " do 
+    expect(ten_minute_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq true
+  end
+
+  it " should return ((['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])) as false" do
+    expect(ten_minute_walk?((['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']))).to eq false
+  end
 
 end
